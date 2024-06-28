3eeef7fcbf007540b7adeaf2f15f4cdbd6e74143
NhongPlus <lop10czodoi@gmail.com>
2024-06-28 10:41:46 +0700

    first commit

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ const operators = document.querySelectorAll(".operator");
 const clear = document.querySelector(".clear");
 const clearAll = document.querySelector(".clearAll");
 const del = document.querySelector(".del");
-const signToggle = document.querySelector(".sign-toggle");
 const seconScreen = document.querySelector(".small");
 const demical = document.querySelector(".demical");
-const addMemory = document.querySelector(".addMemory");
-const local = document.querySelector(".localStorage");
 
 let soThuNhat = 0;
 let soThuHai = 0;
@@ -19,21 +16,21 @@ let manChinh = "";  // màn hình chính
 let manPhu = ""; // màn hình phụ
 let isNum1 = false;
 let isNum2 = false;
-let isOperator = false; // Là + - * /
+let isOperator = false; // cờ toán tử
 let isResult = false;
-let toanTu = true;
-let dauBang = true;
-let history = [];
-
+let dauBang = false; // cờ dấu =
+let giaTriCuoi = 0;
+let giaTriGanCuoi = null;
+let count = false;
 // click số
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function (e) {
-    let a = parseInt(e.target.innerHTML);
-    if (isResult) {
-      ClearMan();
+    let a = e.target.innerHTML;
+    if (isResult) { // nếu mà có dấu kết quả thì khi nhập số thì nó sẽ clear màn đi 
+      ClearAll();
       isResult = false;
     }
-    if (isOperator === false) {
+    if (isOperator === false) { // nếu ko có toán tử thì cho nhập số thứ nhất , còn ko thì ngược lại
       SoDau(a);
       console.log(soThuNhat);
     } else {
@@ -42,17 +39,16 @@ for (let i = 0; i < numbers.length; i++) {
     }
   });
 }
-// click toán tử
+// Click toán tử
 for (let i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", function (e) {
-    const operator = e.target.textContent.trim(); // Bỏ khoảng trắng
-    toanTu;
+    const operator = e.target.textContent.trim();
+    // Xử lý các phép tính đặc biệt
     switch (operator) {
       case 'x2':
         Pow(); // bình phương
         break;
       case '1/x':
-        console.log("suscess");
         Fraction(); // 1 phần x
         break;
       case '+/-':
@@ -62,48 +58,34 @@ for (let i = 0; i < operators.length; i++) {
         PhanTram(); // Percent
         break;
       case '2√x':
-        SquareRoot(); // căn 
+        SquareRoot(); // căn
         break;
       default:
-        isOperator = true; // Cập nhật trạng thái
-        toanTuHienTai = operator; // Lưu toán tử
-        console.log(operator);
-        ClearMan(); // Xóa Màn Hình
+        isOperator = true;
+        toanTuHienTai = operator;
+        ClearMan();
         break;
     }
-    manHinhPhu();
+    // phần này nó là ví dụ nếu mà cộng dồn r sau đó bắt dược giá trị cuối , nếu mà là click trừ thì là lấy giaTriCuoi trừ cho cái sothu2 , nhưng mà bắt sothu2 ở đây bị null
+
+    // if (isResult && !['x2', '1/x', '+/-', '2√x', '%'].includes(operator)) {
+    //   giaTriCuoi = soThuNhat; 
+    //   giaTriGanCuoi = parseFloat(screen.innerHTML); 
+    //   ClearMan(); 
+    //   seconScreen.innerHTML = `${giaTriCuoi} ${operator} ${giaTriGanCuoi}`;
+    //   if (operator === '+') { 
+    //     const final = giaTriCuoi + giaTriGanCuoi;
+    //     screen.innerHTML = final.toString(); 
+    //     Cong();
+    //   }
+    // }
   });
 }
-// thêm vào lịch sử
-addMemory.addEventListener('click' , function(e){
-  if(e.target.textContent === 'M+'){
-    addToHistory(soThuNhat , operators ,soThuHai);
-  }
-})
-// in ra lịch sử
-local.addEventListener("click", function () {
-  if (history.length > 0) {
-    console.log("History:");
-    for (let i = 0; i < history.length; i++) {
-      console.log(`${history[i].num1} ${history[i].operator} ${history[i].num2} = ${history[i].result}`);
-    }
-  } else {
-    console.log("No history available.");
-  }
-});
-//  Add ptu vao mang
-function addToHistory(num1, operator, num2, result) {
-  history.push({
-      num1: num1,
-      operator: operator,
-      num2: num2,
-      result: result
-  });
-}
+
 // Nút bằng
 equal.addEventListener('click', function (e) {
-  if (toanTuHienTai && isOperator && soThuHai !== null) {
-    let dauBang;
+  dauBang = true; // phần này dùng để cộng liên tiếp , nếu mà cứ click cộng tiếp thì nó sẽ gán kết quả cho soThuNhat
+  if (toanTuHienTai && isNum1 && isNum2) {
     switch (toanTuHienTai) {
       case '+':
         Cong();
@@ -120,169 +102,167 @@ equal.addEventListener('click', function (e) {
       default:
         break;
     }
-    manHinhPhu();
-    addToHistory(soThuNhat, toanTuHienTai, soThuHai, result);
   }
 });
-
 
 // Nút Del
 del.addEventListener('click', function () {
   if (manChinh.length > 0) {
     manChinh = manChinh.slice(0, -1);
     screen.innerHTML = manChinh;
+  }
+
+  if (isOperator) {
     if (manChinh !== "") {
-      soThuNhat = parseInt(manChinh);
+      soThuHai = parseFloat(manChinh);
+      isNum2 = true;
+    } else {
+      soThuHai = 0;
+      isNum2 = false;
+    }
+  } else {
+    if (manChinh !== "") {
+      soThuNhat = parseFloat(manChinh);
+      isNum1 = true;
     } else {
       soThuNhat = 0;
-      isOperator = false;
+      isNum1 = false;
     }
   }
 });
+
 // Nút demical
 demical.addEventListener('click', function (e) {
   const demi = e.target.textContent;
-  console.log(demi);
-  if (!manChinh.includes(',')) {
-    manChinh += ',';
+  if (manChinh === "") {
+    manChinh = "0";
+  }
+  if (!manChinh.includes('.')) {
+    manChinh += '.';
     screen.innerHTML = manChinh;
   }
-});
-// Nút CE
-clearAll.addEventListener('click', function () {
-  ClearAll();
-});
-
-// Nút C
-clear.addEventListener('click', function () {
-  ClearMan();
 });
 
 // Số Đầu 
 function SoDau(num) {
+  if (manChinh === "0" && num === "0") { // nếu mà màn chính = 0 ròi mà num = 0 thì ko làm j cả
+    return;
+  }
   manChinh += `${num}`;
-  manChinh = manChinh.replace(',', '.'); 
+  manChinh = manChinh.replace(',', '.');
   screen.innerHTML = manChinh;
-  soThuNhat = parseFloat(manChinh); 
+  soThuNhat = parseFloat(manChinh);
   isNum1 = true;
 }
 // Số Cuối
 function SoCuoi(num) {
-  if (isOperator === true) {
+  if (manChinh === "0" && num === "0") {
+    return;
+  }
+  if (isOperator === true) { // có toán tử rồi thì nhập số thứ 2
     manChinh += `${num}`;
-    manChinh = manChinh.replace(',', '.'); 
+    manChinh = manChinh.replace(',', '.');
     screen.innerHTML = manChinh;
     soThuHai = parseFloat(manChinh);
     isNum2 = true;
   }
 }
-
-function manHinhPhu() {
-  if (toanTuHienTai && isOperator && soThuHai !== null) {
-    manPhu = `${tong - soThuNhat} ${toanTuHienTai} ${soThuHai} =`;
-    seconScreen.innerHTML = manPhu;
-  } else if (isOperator) {
-    manPhu = `${soThuNhat} ${toanTuHienTai}`;
-    seconScreen.innerHTML = manPhu;
-  } else {
-    manPhu = `${soThuNhat}`;
-    seconScreen.innerHTML = manPhu;
-  }
-}
-
-// Xóa màn hình
-function ClearMan() {
-  manChinh = '';
-  screen.innerHTML = '';
-
-}
-
-function ClearAll() {
-  ClearMan();
-  manPhu = '';
-  seconScreen.innerHTML = '';
-  soThuNhat = 0;
-  soThuHai = null;
-  toanTuHienTai = null;
-  isNum1 = false;
-  isNum2 = false;
-  isOperator = false;
-}
 // Phép Cộng
 function Cong() {
+  seconScreen.innerHTML = `${soThuNhat} + ${soThuHai} = ` // in ra màn phụ
   const tong = soThuNhat + soThuHai;
   screen.innerHTML = tong;
   manChinh = tong.toString();
-  soThuNhat = tong
-  isResult = true; // Đánh dấu rằng kết quả cuối cùng đã được tính
-  return tong;
+  isResult = true;
+  if (dauBang === true) { // nếu click tiếp dấu = thì hiển gán soThuNhat = tong để cộng liên tiếp
+    soThuNhat = tong;
+  }
+  
 }
-
 // Phép Trừ
 function Tru() {
+  seconScreen.innerHTML = `${soThuNhat} - ${soThuHai} = `
   const hieu = soThuNhat - soThuHai;
   screen.innerHTML = hieu;
   manChinh = hieu.toString();
-  soThuNhat = hieu;
-  isResult = true; // Đánh dấu rằng kết quả cuối cùng đã được tính
-  return hieu;
+  isResult = true;
+  if (dauBang === true) {
+    soThuNhat = hieu;
+  }
 }
-
 // Phép Nhân
 function Nhan() {
+  seconScreen.innerHTML = `${soThuNhat} * ${soThuHai} = `
   const tich = soThuNhat * soThuHai;
   screen.innerHTML = tich;
   manChinh = tich.toString();
-  soThuNhat = tich;
-  isResult = true; // Đánh dấu rằng kết quả cuối cùng đã được tính
-  return tich;
+  isResult = true;
+  if (dauBang === true) {
+    soThuNhat = tich;
+  }
 }
 
 // Phép Chia
 function Chia() {
+  seconScreen.innerHTML = `${soThuNhat} / ${soThuHai} = `
   console.log(soThuHai);
   if (soThuHai !== 0) {
     const thuong = soThuNhat / soThuHai;
     screen.innerHTML = thuong;
     manChinh = thuong.toString();
     soThuNhat = thuong;
-    isResult = true; // Đánh dấu rằng kết quả cuối cùng đã được tính
+    isResult = true;
   } else {
-    screen.innerHTML = "Error"; // Xử lý khi chia cho 0
+    screen.innerHTML = "Cannot divide by zero";
     manChinh = "";
   }
 }
-
 // Phần Trăm
 function PhanTram() {
-  const phanTram = soThuNhat / 100;
-  screen.innerHTML = phanTram;
-  soThuNhat = phanTram;
-  return phanTram;
-}
-
-// Bình Phương
-function Pow() {
-  if (isResult) {
-    // Nếu kết quả đã được tính, áp dụng bình phương cho kết quả đó
-    let ketQua = parseFloat(screen.innerHTML);
-    if (ketQua < 0) {
+  if (isResult) { // nếu số phần trăm là kết quả
+    let ketQua = parseInt(screen.innerHTML);
+    seconScreen.innerHTML = `${ketQua}`
+    ketQua = ketQua / 100;
+    screen.innerHTML = ketQua;
+    manChinh = ketQua.toString();
+    soThuNhat = ketQua; // gán tiếp nếu tiếp tục bình phương
+    isResult = false;
+  } else { // nếu là sothunhat hoặc so thu 2
+    const soHienTai = parseInt(manChinh);
+    if (soHienTai < 0) {
       screen.innerHTML = "Undefined";
       manChinh = "";
     } else {
-      ketQua = Math.pow(ketQua, 2);
-      screen.innerHTML = ketQua;
-      manChinh = ketQua.toString();
-      soThuNhat = ketQua; // Cập nhật lại soThuNhat với kết quả mới bình phương
-      isResult = false; // Sau khi bình phương, không còn là kết quả cuối nữa
+      const phanTram = soHienTai / 100
+      seconScreen.innerHTML = `${soHienTai}`
+      screen.innerHTML = phanTram;
+      manChinh = phanTram.toString();
+      if (isOperator) {
+        soThuHai = phanTram;
+      } else {
+        soThuNhat = phanTram;
+      }
     }
-  } else {
+  }
+}
+// Hàm bình phương
+function Pow() {
+  if (isResult) { // nếu số pow là kết quả
+    let ketQua = parseFloat(screen.innerHTML);
+    seconScreen.innerHTML = `sqr(${ketQua})`
+    ketQua = Math.pow(ketQua, 2);
+    screen.innerHTML = ketQua;
+    manChinh = ketQua.toString();
+    soThuNhat = ketQua; // gán tiếp nếu tiếp tục bình phương
+    isResult = false;
+  } else { // nếu là sothunhat hoặc so thu 2
     const soHienTai = parseFloat(manChinh);
     if (soHienTai < 0) {
       screen.innerHTML = "Undefined";
       manChinh = "";
     } else {
       const binhPhuong = Math.pow(soHienTai, 2);
+      seconScreen.innerHTML = `sqr(${soHienTai})`
       screen.innerHTML = binhPhuong;
       manChinh = binhPhuong.toString();
       if (isOperator) {
@@ -293,25 +273,24 @@ function Pow() {
     }
   }
 }
-
-
 // Căn 
 function SquareRoot() {
   if (isResult) {
-    // Nếu kết quả đã được tính, áp dụng căn bậc hai cho kết quả đó
     let ketQua = parseFloat(screen.innerHTML);
+    seconScreen.innerHTML = `√(${ketQua})`
     if (ketQua < 0) {
-      screen.innerHTML = "Undefined";
+      screen.innerHTML = "Invalid input";
       manChinh = "";
     } else {
       ketQua = Math.sqrt(ketQua);
       screen.innerHTML = ketQua;
       manChinh = ketQua.toString();
-      soThuNhat = ketQua; // Cập nhật lại soThuNhat với kết quả mới căn bậc hai
-      isResult = false; // Sau khi căn bậc hai, không còn là kết quả cuối nữa
+      soThuNhat = ketQua;
+      isResult = false;
     }
   } else {
     const soHienTai = parseFloat(manChinh);
+    seconScreen.innerHTML = `√(${soHienTai})`
     if (soHienTai < 0) {
       screen.innerHTML = "Undefined";
       manChinh = "";
@@ -327,26 +306,26 @@ function SquareRoot() {
     }
   }
 }
-
 // Hàm 1 nghịch đảo
 function Fraction() {
   if (isResult) {
-    // Nếu kết quả đã được tính, áp dụng nghịch đảo cho kết quả đó
     let ketQua = parseFloat(screen.innerHTML);
+    seconScreen.innerHTML = `1/(${ketQua})`
     if (ketQua === 0) {
-      screen.innerHTML = "Error"; // Không thể chia cho 0
+      screen.innerHTML = "Cannot divide by zero";
       manChinh = "";
     } else {
       ketQua = 1 / ketQua;
       screen.innerHTML = ketQua;
       manChinh = ketQua.toString();
-      soThuNhat = ketQua; // Cập nhật lại soThuNhat với kết quả mới nghịch đảo
-      isResult = false; // Sau khi nghịch đảo, không còn là kết quả cuối nữa
+      soThuNhat = ketQua;
+      isResult = false;
     }
   } else {
     const soHienTai = parseFloat(manChinh);
+    seconScreen.innerHTML = `1/(${soHienTai})`
     if (soHienTai === 0) {
-      screen.innerHTML = "Error"; // Không thể chia cho 0
+      screen.innerHTML = "Cannot divide by zero";
       manChinh = "";
     } else {
       const nghichDao = 1 / soHienTai;
@@ -360,21 +339,20 @@ function Fraction() {
     }
   }
 }
-
 // Hàm đổi dấu
 function toggleSign() {
-  if (isResult) {
-    // Nếu kết quả đã được tính, áp dụng đổi dấu cho kết quả đó
+  if (isResult) { // nếu là kết quả 
     let ketQua = parseFloat(screen.innerHTML);
+    seconScreen.innerHTML = `negate(${ketQua})`
     ketQua = -ketQua;
     screen.innerHTML = ketQua;
     manChinh = ketQua.toString();
-    soThuNhat = ketQua; // Cập nhật lại soThuNhat với kết quả mới đổi dấu
-    isResult = false; // Sau khi đổi dấu, không còn là kết quả cuối nữa
-  } else {
-    // Nếu chưa tính kết quả, áp dụng đổi dấu cho số hiện tại
+    soThuNhat = ketQua;
+    isResult = false;
+  } else { // nếu là sothunhat hoặc sothu2
     const soHienTai = parseFloat(manChinh);
-    if (!isNaN(soHienTai)) {
+    seconScreen.innerHTML = `negate(${soHienTai})`
+    if (soHienTai !== 0) {
       const doiDau = soHienTai * -1;
       screen.innerHTML = doiDau;
       manChinh = doiDau.toString();
@@ -384,5 +362,38 @@ function toggleSign() {
         soThuNhat = doiDau;
       }
     }
+    else {
+      const doiDau = soHienTai; // 0 đổi dấu thì vẫn thế
+      screen.innerHTML = doiDau;
+    }
   }
 }
+// Xóa màn hình
+function ClearMan() {
+  manChinh = '';
+  screen.innerHTML = '';
+}
+// Xóa all
+function ClearAll() {
+  ClearMan();
+  manPhu = '';
+  seconScreen.innerHTML = '';
+  soThuNhat = 0;
+  soThuHai = null;
+  toanTuHienTai = null;
+  isNum1 = false;
+  isNum2 = false;
+  isOperator = false;
+}
+// Nút CE
+clearAll.addEventListener('click', function () {
+  ClearAll();
+});
+// Nút C
+clear.addEventListener('click', function () {
+  ClearMan();
+});
+
+// khi cộng dồn các số vào r , giả sử - cho 1 số nào đó => phải đặt cái sổ ở MÀN CHÍNH làm số thứ nhất => phải lược cho các toán tử còn lại
+// nhưng mà toán tử nằm ở operator thì nếu mà chỉnh + - * / thì sẽ bị lỗi
+// Chỉnh ở phía các toán tử + - * / , với mỗi hàm truyền vào 1 toán tử , nếu khác toán tử đó thì set nó = sothunhat
